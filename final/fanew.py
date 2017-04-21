4c8b234c25b55de674e8bcb1c16c67de5c66bae9
Joseph Burke <jburke@scrippsnetworks.com>
2017-04-21 16:24:57 -0400

    fa with hamming interations

--- final/fanew.py
+++ final/fanew.py
@@ -1,17 +1,18 @@
 from myhdl import Signal, always_comb, Simulation,delay,instance,intbv,bin,traceSignals,toVerilog
+from heapq import nsmallest
 
 stuck = []
 
-def full_adder(A,B,C_in,K,Sum_out,C_out):
+def full_adder(A,B,C_in,K1,K2,K3,Sum_out,C_out):
     """full adder circuit
     {A,B,C} --- inputs
     {Sum_out,C_out} -- outputs
     """
     @always_comb
     def logic():
-        Sig1= A ^ B ^ K
-        Sig2 = Sig1 & C_in
-        Sig3 = A & B
+        Sig1= A ^ B ^ K1
+        Sig2 = (Sig1 & C_in) ^ K2
+        Sig3 = (A & B) ^ K3 
         Sum_out.next = Sig1 ^ C_in
         C_out.next = Sig2 or Sig3
     return logic
@@ -20,63 +21,78 @@ from random import randrange
 
 def testBench_FA(flag=0):
     """The test bench mark to test the full adder hardware module"""
-    A,B,C_in,K,Sum_out,C_out = [Signal(bool(0)) for i in range(6)]
-    FA_inst = full_adder(A,B,C_in,K,Sum_out,C_out)
+    A,B,C_in,K1,K2,K3,Sum_out,C_out = [Signal(bool(0)) for i in range(8)]
+    FA_inst = full_adder(A,B,C_in,K1,K2,K3,Sum_out,C_out)
     
     @instance
     def simulate():
-        beforeS = []
-        beforeC = []
-        before = []
-        
+        ham = {}
+        for p in range(8):
+            keyset = format(p,"03b")
+            
+            beforeS = []
+            beforeC = []
+            before = []
         #print('A  B  Cin  SumOut  Cout')
-        for i in range(8):
-            set = format(i,"03b")
-            A.next = int(set[0])
-            B.next = int(set[1])
-            C_in.next = int(set[2])
-            K.next = 0
-            yield delay(10)
-            #print ('{}  {}  {}    {}        {} BEFORE'.format(bin(A,1),bin(B,1),bin(C_in,1),bin(Sum_out,1),bin(C_out,1)))
-            beforeS.append(int(bin(Sum_out)))
-            beforeC.append(int(bin(C_out)))
+            for i in range(8):
+                K1.next = 0
+                K2.next = 0
+                K3.next = 0
+                set = format(i,"03b")
+                A.next = int(set[0])
+                B.next = int(set[1])
+                C_in.next = int(set[2])
+                yield delay(10)
+                #print ('{}  {}  {}    {}        {} BEFORE'.format(bin(A,1),bin(B,1),bin(C_in,1),bin(Sum_out,1),bin(C_out,1)))
+                beforeS.append(int(bin(Sum_out)))
+                beforeC.append(int(bin(C_out)))
 
-        before = beforeS + beforeC
-        print('Before:')
-        print(beforeS)
-        print(beforeC)
-        print(before)
+            before = beforeS + beforeC
+            print('Before:')
+            print(beforeS)
+            print(beforeC)
+            print(before)
 
-        K.next = 1
-        afterS = []
-        afterC = []
-        after = []
+            K1.next = int(keyset[0])
+            K2.next = int(keyset[1])
+            K3.next = int(keyset[2])
+            afterS = []
+            afterC = []
+            after = []
 
-        for i in range(8):
-            set = format(i,"03b")
-            A.next = int(set[0])
-            B.next = int(set[1])
-            C_in.next = int(set[2])
-            yield delay(10)
-            #print ('{}  {}  {}    {}        {} BEFORE'.format(bin(A,1),bin(B,1),bin(C_in,1),bin(Sum_out,1),bin(C_out,1)))
-            afterS.append(int(bin(Sum_out)))
-            afterC.append(int(bin(C_out)))
-        
-        after = afterS + afterC
-        print('After:')
-        print(afterS)
-        print(afterC)
-        print(after)
+            for i in range(8):
+                set = format(i,"03b")
+                A.next = int(set[0])
+                B.next = int(set[1])
+                C_in.next = int(set[2])
+                yield delay(10)
+                afterS.append(int(bin(Sum_out)))
+                afterC.append(int(bin(C_out)))
+            
+            after = afterS + afterC
+            print('After:')
+            print(afterS)
+            print(afterC)
+            print(after)
 
-        t = []
+            t = []
 
-        for i in range(len(before)):
-            if before[i] != after[i]:
-                t.append(before[i])
+            for i in range(len(before)):
+                if before[i] != after[i]:
+                    t.append(before[i])
 
-        hamming = len(t)/len(before)
-        print('Hamming = {}'.format(hamming))
+            hamming = len(t)/len(before)
+            print('Key1: {} Key2: {} Key3: {}'.format(keyset[0],keyset[1],keyset[2]))
+            print('Hamming = {}'.format(hamming))
+            ham[hamming] = p
 
+        print('Hamming List:')
+        print(ham)
+
+        smallest = nsmallest(1, ham, key=lambda x: abs(x-0.5))
+        key = format(ham[smallest[0]],"03b")
+        print('Key: {}'.format(key))
+        print('Hamming: {}'.format(smallest))
 
     return FA_inst,simulate
 
