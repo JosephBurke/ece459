43d503ca20e730ff55712235ebf1cd63b5cdc2f1
Joseph Burke <jburke@scrippsnetworks.com>
2017-04-22 16:18:23 -0400

    t

--- project/6bit_fa.py
+++ project/6bit_fa.py
@@ -1,9 +1,10 @@
 from myhdl import Signal, always_comb, Simulation,delay,instance,intbv,bin,traceSignals,toVerilog
 from heapq import nsmallest
+from myhdl import *
 
 stuck = []
 
-def full_adder(A,B,A1,B1,C_in,K1,K2,K3,K4,K5,K6,Sum_out,Sum1_out,C_out):
+def full_adder(A,B,A1,B1,A2,B2,C_in,K1,K2,K3,K4,K5,K6,K7,K8,K9,Sum_out,Sum1_out,Sum2_out,C_out):
     """full adder circuit
     {A,B,C} --- inputs
     {Sum_out,C_out} -- outputs
@@ -11,62 +12,73 @@ def full_adder(A,B,A1,B1,C_in,K1,K2,K3,K4,K5,K6,Sum_out,Sum1_out,C_out):
     @always_comb
     def logic():
         Sig1= A ^ B ^ K1
-        Sig2 = (Sig1 & C_in) ^ K2
+        Sig2 = ((Sig1 & C_in) ^ K2)
         Sig3 = (A & B) ^ K3 
         Sum_out.next = Sig1 ^ C_in
-        C = Sig2 or Sig3
+        C = (Sig2 | Sig3)
         Sig4 = A1 ^ B1 ^ K4
-        Sig5 = (Sig4 & C) ^ K5
+        Sig5 = ((Sig4 & C) ^ K5)
+        Sig5 = not Sig5
         Sig6 = (A1 & B1) ^ K6
         Sum1_out.next = Sig4 ^ C
-        C_out.next = (Sig5 or Sig6)
+        C1 = (Sig5 | Sig6) 
+        Sig7 = (A2 ^ B2 ^ K7)
+        Sig7 = not Sig7
+        Sig8 = ((Sig7 & C) ^ K8)
+        Sig9 = (A2 & B2) ^ K9
+        C_out.next = Sig8 | Sig9
     return logic
 
 from random import randrange
 
 def testBench_FA(flag=0):
     """The test bench mark to test the full adder hardware module"""
-    A,B,A1,B1,C_in,K1,K2,K3,K4,K5,K6,Sum_out,Sum1_out,C_out = [Signal(bool(0)) for i in range(14)]
-    FA_inst = full_adder(A,B,A1,B1,C_in,K1,K2,K3,K4,K5,K6,Sum_out,Sum1_out,C_out)
+    A,B,A1,B1,A2,B2,C_in,K1,K2,K3,K4,K5,K6,K7,K8,K9,K11,Sum_out,Sum1_out,Sum2_out,C_out = [Signal(bool(0)) for i in range(21)]
+    FA_inst = full_adder(A,B,A1,B1,A2,B2,C_in,K1,K2,K3,K4,K5,K6,K7,K8,K9,Sum_out,Sum1_out,Sum2_out,C_out)
     
     @instance
     def simulate():
         ham = {}
         hamSum = 0
-        for p in range(64):
-            keyset = format(p,"06b")
+        bitW = 0
+        bitC = 0
+        for p in range(512):
+            keyset = format(p,"09b")
             
             beforeS = []
             beforeS1 = []
+            beforeS2 = []
             beforeC = []
             before = []
         #print('A  B  Cin  SumOut  Cout')
-            for i in range(32):
+            for i in range(128):
                 K1.next = 0
                 K2.next = 0
                 K3.next = 0
                 K4.next = 0
                 K5.next = 0
                 K6.next = 0
-                set = format(i,"05b")
+                K7.next = 0
+                K8.next = 0
+                K9.next = 0
+                set = format(i,"07b")
                 A.next = int(set[0])
                 B.next = int(set[1])
                 C_in.next = int(set[2])
                 A1.next = int(set[3])
-                B1.next = int(set[4]) 
+                B1.next = int(set[4])
+                A2.next = int(set[5])
+                B2.next = int(set[6])
 
                 yield delay(10)
                 #print ('{}  {}  {}    {}        {} BEFORE'.format(bin(A,1),bin(B,1),bin(C_in,1),bin(Sum_out,1),bin(C_out,1)))
                 beforeS.append(int(bin(Sum_out)))
                 beforeS1.append(int(bin(Sum1_out)))
+                beforeS2.append(int(bin(Sum2_out)))
                 beforeC.append(int(bin(C_out)))
 
-            before = beforeS + beforeS1 + beforeC
-            print('Sum0:')
-            print(beforeS)
-            print('Sum1:')
-            print(beforeS1)
-            print('Carry:')
+            before = beforeS + beforeS1 + beforeS2 + beforeC
+            print('Before:')
             print(beforeC)
             
             print(before)
@@ -77,37 +89,48 @@ def testBench_FA(flag=0):
             K4.next = int(keyset[3])
             K5.next = int(keyset[4])
             K6.next = int(keyset[5])
+            K7.next = int(keyset[6])
+            K8.next = int(keyset[7])
+            K9.next = int(keyset[8])
             afterS = []
             afterS1 = []
             afterC = []
+            afterS2 = []
             after = []
 
-            for i in range(32):
-                set = format(i,"05b")
+            for i in range(128):
+                set = format(i,"07b")
                 A.next = int(set[0])
                 B.next = int(set[1])
                 C_in.next = int(set[2])
                 A1.next = int(set[3])
-                B1.next = int(set[4]) 
+                B1.next = int(set[4])
+                A2.next = int(set[5])
+                B2.next = int(set[6])
                 yield delay(10)
                 afterS.append(int(bin(Sum_out)))
                 afterS1.append(int(bin(Sum1_out)))
+                afterS2.append(int(bin(Sum2_out)))
                 afterC.append(int(bin(C_out)))
+                
             
-            after = afterS + afterS1 + afterC
+            after = afterS + afterS1 + afterS2 + afterC
             print('After:')
-            print(afterS)
             print(afterC)
+
             print(after)
 
             t = []
 
             for i in range(len(before)):
+                bitC += 1
                 if before[i] != after[i]:
+                    bitW += 1
                     t.append(before[i])
 
             hamming = len(t)/len(before)
-            print('Key1: {} Key2: {} Key3: {}'.format(keyset[0],keyset[1],keyset[2]))
+            for index, key in enumerate(keyset):
+                print('Key{}: {}'.format(index,key))
             print('Hamming = {}'.format(hamming))
             ham[hamming] = p
             hamSum += hamming
@@ -116,10 +139,12 @@ def testBench_FA(flag=0):
         print(ham)
 
         smallest = nsmallest(1, ham, key=lambda x: abs(x-0.5))
-        key = format(ham[smallest[0]],"06b")
+        key = format(ham[smallest[0]],"09b")
         print('Best Key Combination: {}'.format(key))
         print('Hamming Distance: {}'.format(smallest))
-        print('Ave Hamming = {}'.format(hamSum/63))
+        print('Ave Hamming = {}'.format(hamSum/511))
+        print('Wrong {} / {}'.format(bitW,bitC))
+        print('Percent: {}'.format(bitW/bitC))
 
     return FA_inst,simulate
 
